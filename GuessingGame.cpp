e7ee16dd57b91a8d6719ced1e5691a4ae8ded7e2
JHOWELL <atlas3141@gmail.com>
2016-09-09 13:42:39 -0700

    "Everything but the while loop to add stuff for g"

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
 int main(int argc, char** argv){
-  return 0;
+  srand(time(NULL));
+  int number = rand() %101;
+  int guess = -1;
+  int guessCount = 0;
+  cout << "Im thinking of a number between 0 and 100" << endl;
+  cout << "Try to guess it!" << endl;
 }
