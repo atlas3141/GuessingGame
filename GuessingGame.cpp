538361d63e4a36f70a1fb13a2d85aa358d3ad1a4
JHOWELL <atlas3141@gmail.com>
2016-09-09 20:40:52 -0700

    Asks if you want to play again

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 using namespace std;
 
-int main(int argc, char** argv){
-  srand(time(NULL));
-  int number = rand() %101;
+int main(){
+  srand(time(NULL)); //sets the random seed
+  int number = rand() %101; //creates the random number
+  char input[2];
   int guess = -1;
   int guessCount = 1;
+  bool playing = true;
   cout << "Im thinking of a number between 0 and 100" << endl;
   cout << "Try to guess it!" << endl;
-  while(guess != number){
-    cin >> guess;
+  while(playing == true){ //main game loop
+    cin >> guess; //asks for a number
     if (guess < number){
       cout << "That number is lower than mine" << endl;
     }
@@ -20,8 +23,19 @@ int main(int argc, char** argv){
     }
     else{
       cout << "You got it" << endl;
-      cout << "It took you  " << guessCount << " tries" << endl; 
+      cout << "It took you  " << guessCount << " tries" << endl;\
+      cout << "Do you want to play again (y/n)" << endl;
+      cin >> input;
+      cout << input << endl;
+      if(strcmp(input,"y")){ //ask if they want to play agian
+	  playing = false;
+      }
+      else{
+	cout << "Im thinking of a number between 0 and 100" << endl;
+	cout << "Try to guess it!" << endl;
+      }
     }
-    guessCount++;
+  guessCount++;
   }
 }
+
